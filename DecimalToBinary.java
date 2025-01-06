220310c8eb7b3aab66491de0a482fba98ee49dbe
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-01-06 23:55:07 +0530

    Convert the Decimal to binary

---
+++ DecimalToBinary.java
@@ -0,0 +1,8 @@
+public class DecimalToBinary {
+
+    public static void main(String[] args){
+        int x=5;
+        System.out.println(Integer.toBinaryString(x)); // these method converts the decimal to binary
+    }
+    
+}
