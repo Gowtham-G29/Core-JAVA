3e024cff3639ca0138f36598a8488ab03f357674
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-01-06 21:46:55 +0530

    Read the user input using scanner

---
+++ KeyboardRead.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class KeyboardRead {
+
+    public static void main(String[] args){
+        String name;
+        
+        Scanner s=new Scanner(System.in);
+
+        name=s.nextLine();
+
+        System.out.println("Name is: "+name);
+
+    }
+    
+}
