6eee6626d0f0753ae78f455cfa057fd2479aac2d
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-01-07 22:51:56 +0530

    Operators and Expressions Practice problems

---
+++ Practice problems/IncrementAndDecrement.java
@@ -0,0 +1,11 @@
+
+public class IncrementAndDecrement {
+
+    public static void main(String[] args){
+
+        int x=5,y=4,z;
+        z=2*x++ + 3*++y;  //2*5 + 3*5
+        System.out.println(x+" "+y+" "+z);
+    }
+    
+}
