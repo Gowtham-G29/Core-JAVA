ae2fae996260e1e0428ae8f8063caeaeca639802
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-01-07 19:13:02 +0530

    Arithmetic operations

---
+++ Arithmetic.java
@@ -0,0 +1,23 @@
+public class Arithmetic {
+
+    public static void main(String[] args){
+
+        // float a=14.3f,b=3.2f;
+
+        // float c=(float)a/b;
+
+        // float r=a%b;
+
+        // System.out.println(c);
+        // System.out.println(r);
+
+        byte a=10;
+        short b=15;
+
+        int c=a+b;
+
+        System.out.println(c);
+    }
+    
+}
+ 
